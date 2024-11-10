872da596d3c7836b1918f721641cf78a7bece8df
Rahullkumr <kumarrahul771996@gmail.com>
2024-11-10 18:44:04 +0530

    chore: upload web-version of GuessIndia game

--- README.md
+++ README.md
@@ -1,56 +1,18 @@
-# Indian States and UTs Guessing Game
+# Indian States and UTs Guessing Game (web-version)
 
-This is an interactive Python game where users attempt to name all 28 states and 8 Union Territories (UTs) of India. The program will show an image of the map of India and ask users to input state and UTs names. If a correct name is entered, it will be displayed on the map. The game also generates a file with the names of any states the player missed.
-
-   > built using `Python`, `Pandas` and `Turtle`
-
-![final output](./guessIndia.png)
-
-## Checkout the web version of this game [here]()
+This is an interactive game where users attempt to name all 28 states and 8 Union Territories (UTs) of India. The game will show an image of the map of India and ask users to input state and UT names. If a correct name is entered, it will be displayed on the map. The game also tells the user the names of any states or UTs he/she missed.
 
    > built using `HTML`, `CSS` and `JavaScript`
 
+
+![final UI](./guessIndia_ui.png)
+
+## Let's see how much you score [click here]()
+
 ## Features
-- Displays a map of India with a blank outline of the states and UTs.
+- Displays the map of India with a blank outline of the states and UTs.
 - Allows users to guess states' and UTs' names and shows correct guesses on the map.
-- Generates a `names_you_missed.csv` file listing any states or UTs not guessed during the game.
-
-## Prerequisites
-1. **Python 3.x**: Ensure Python is installed. You can download it from [Python.org](https://www.python.org/).
-2. **Pandas**: Install the `pandas` library for data manipulation:
-   ```
-   pip install pandas
-   ```
-3. **Turtle Graphics**: The `turtle` module is a standard Python library, so no additional installation is needed.
-
-## Project Structure
-- `main.py`: Main game file containing the code to run the game.
-- `states_and_uts.csv`: CSV file with state and UT names and their coordinates.
-- `map_of_India.gif`: Image of the India map outline used as the game background.
-
-## Getting Started
-
-1. **Clone the repository**: Clone the project to your local machine using Git.
-   ```
-   git clone https://github.com/Rahullkumr/GuessIndianStatesAndUTs.git
-   ```
-
-2. **Navigate to the project directory**:
-   ```
-   cd GuessIndianStatesAndUTs
-   ```
-
-3. **Ensure all required files are in the directory**:
-   - `main.py`
-   - `states_and_uts.csv`
-   - `map_of_India.gif`
-
-## Running the Game
-
-To start the game, execute the following command:
-```
-python main.py
-```
+- Generates the output listing any states or UTs not guessed during the game.
 
 ## How to Play
 
@@ -58,11 +20,7 @@ python main.py
 2. Type the name of a state or UT and press Enter.
 3. If the state/UT name is correct, it will appear on the map at the state's or UT's location.
 4. Type "Exit" if you want to stop the game early.
-   - When you exit, a `names_you_missed.csv` file will be created in the project directory, listing any name you did not guess.
-
-## Note
-
-- Ensure that `map_of_India.gif` is present in the same directory as the code. If it's missing, the game will not display the background map.
+   - When you exit, output will be visible on the right side, listing any name you did not guess.
 
 ## Author
 - Created by me [Rahul Kumar](https://github.com/rahullkumr)
---
+++ guessIndia.js
@@ -0,0 +1,119 @@
+// 28 states and 8 UTs 
+
+const statesAndUTs = {
+    "Andhra Pradesh": [615, 615],
+    "Arunachal Pradesh": [940, 240],
+    "Assam": [920, 272],
+    "Bihar": [770, 300],
+    "Chhattisgarh": [690, 410],
+    "Goa": [492, 575],
+    "Gujarat": [427, 375],
+    "Haryana": [550, 205],
+    "Himachal Pradesh": [570, 140],
+    "Jharkhand": [755, 355],
+    "Karnataka": [520, 635],
+    "Kerala": [553, 729],
+    "Madhya Pradesh": [550, 383],
+    "Maharashtra": [490, 470],
+    "Manipur": [980, 315],
+    "Meghalaya": [900, 300],
+    "Mizoram": [958, 370],
+    "Nagaland": [995, 275],
+    "Odisha": [740, 445],
+    "Punjab": [530, 165],
+    "Rajasthan": [471, 287],
+    "Sikkim": [845, 250],
+    "Tamil Nadu": [590, 700],
+    "Telangana": [592, 528],
+    "Tripura": [930, 350],
+    "Uttar Pradesh": [635, 285],
+    "Uttarakhand": [633, 200],
+    "West Bengal": [820, 375],
+
+    "Chandigarh": [560, 180],
+    "Lakshadweep": [432, 721],
+    "Delhi": [577, 222],
+    "Puducherry": [640, 670],
+    "Jammu And Kashmir": [522, 95],
+    "Ladakh": [571, 62],
+    "Andaman And Nicobar Islands": [800, 640],
+    "Dadra And Nagar Haveli And Daman And Diu": [360, 465],
+}
+const container = document.querySelector('.container');
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const missedStatesBox = document.querySelector('.missedStatesBox')
+const missedStatesContainer = document.querySelector('.missedStatesContainer')
+let correct_guess_heading = document.getElementById('correct_guess_heading')
+let missedNamesList = Object.keys(statesAndUTs); // remove each correct guesses
+let correctlyGuessedNamesList = [];
+
+// converting guessedName into titlecase (first letter of every word capitalized)
+let toTitleCase = (userInputValue) =>{
+    return userInputValue
+    .toLowerCase()
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+button.addEventListener('click', (e)=>{
+    let guessedName = toTitleCase(input.value.trim());
+    input.value = '';
+    let x = null;
+    let y = null;
+
+    for(let i of Object.keys(statesAndUTs)){
+        if(guessedName === 'Exit'){
+            button.disabled = true;
+            missedNamesList.forEach((val)=>{
+                let missedNameHeading = document.createElement('h5');
+                missedStatesContainer.append(missedNameHeading);
+                missedNameHeading.innerHTML = `<h5 style="font-size: 14px;color: gainsboro;"> ${val} </h5>`;
+            });
+            // CHANGE TO VISIBLE
+            missedStatesBox.style.display = 'flex';
+            break;
+        }
+        // TODO: HOW WILL U MANAGE REPETATIONS (if one enters same value 2nd and 3rd time, then ?)
+        if(guessedName === i ){
+            correctlyGuessedNamesList.push(guessedName);
+            let correct_guesses_count = correctlyGuessedNamesList.length
+            correct_guess_heading.innerText = `${correct_guesses_count}/36 Names Correct`;
+            x = statesAndUTs[guessedName][0];
+            y = statesAndUTs[guessedName][1];
+            // span for showing guessed name on the map
+            let guessedNameContainer = document.createElement('span');
+            document.body.append(guessedNameContainer);
+            if(guessedName === 'Dadra And Nagar Haveli And Daman And Diu'){
+                guessedNameContainer.innerHTML = `
+                <span style="
+                    text-align: center;
+                    font-size: 10px;
+                    position: absolute;
+                    top: ${y}px;
+                    left: ${x}px">  ${"Dadra And Nagar Haveli <br/>And Daman And Diu"}
+                </span>`;
+            }else if(guessedName === 'Andaman And Nicobar Islands'){
+                // just for the pin, ye likhna pada
+                guessedNameContainer.innerHTML = `
+                <span style="
+                    font-size: 14px;
+                    position: absolute;
+                    top: ${y}px;
+                    left: ${x}px">  ${guessedName+"📍"};    
+                </span>`;
+            }else{
+                guessedNameContainer.innerHTML = `
+                <span style="
+                    font-size: 14px;
+                    position: absolute;
+                    top: ${y}px;
+                    left: ${x}px">  ${"📍"+guessedName}
+                </span>`;
+            }
+            // remove each correctly guessed name from missedNamesList
+            missedNamesList = missedNamesList.filter(val => val !== `${guessedName}`);
+        }
+    }
+});
